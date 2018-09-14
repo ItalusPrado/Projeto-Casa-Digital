e75ebd6305cb885221cd7c222f34431af66ed894
ItalusPrado <italusprado@acad.ifce.edu.br>
2018-09-14 09:06:47 -0300

    iniciando projeto com pods

--- CasaDigital/CasaDigital/ViewController.swift
+++ CasaDigital/CasaDigital/ViewController.swift
@@ -1,25 +1,87 @@
-//
-//  ViewController.swift
-//  CasaDigital
-//
-//  Created by Italus Rodrigues do Prado on 13/09/2018.
-//  Copyright © 2018 Italus Rodrigues do Prado. All rights reserved.
-//
-
 import UIKit
+import SwiftMQTT
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var ledSwitch: UISwitch!
+    @IBOutlet weak var tvSwitch: UISwitch!
+    @IBOutlet weak var arSwitch: UISwitch!
+    @IBOutlet weak var lampSwitch: UISwitch!
+    
+    var mqttSession: MQTTSession?
+    //let locationManager = LocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        connectMqtt()
     }
-
-
+    
+    @IBAction func activateDispositive(_ sender: UISwitch) {
+        switch sender {
+        case ledSwitch:
+            if sender.isOn{
+                sendMqttMessage(string: "Ativar LED")
+            } else {
+                sendMqttMessage(string: "Desativar LED")
+            }
+        case tvSwitch:
+            if sender.isOn{
+                sendMqttMessage(string: "Ativar TV")
+            } else {
+                sendMqttMessage(string: "Desativar TV")
+            }
+        case arSwitch:
+            if sender.isOn{
+                sendMqttMessage(string: "Ativar Ar")
+            } else {
+                sendMqttMessage(string: "Desativar Ar")
+            }
+        case lampSwitch:
+            if sender.isOn{
+                sendMqttMessage(string: "Ativar Lampada")
+            } else {
+                sendMqttMessage(string: "Desativar Lampada")
+            }
+        default:
+            print("Erro, switch não encontrado")
+        }
+    }
+    
+    func connectMqtt(){
+        DispatchQueue.async(<#T##DispatchQueue#>)
+        self.mqttSession = MQTTSession(
+            host: "ifce.sanusb.org",
+            port: 1883,
+            clientID: "swiftItalus", // must be unique to the client
+            cleanSession: true,
+            keepAlive: 15,
+            useSSL: false
+        )
+        self.mqttSession!.connect { (result, error) in
+            if result == false {
+                self.statusLabel.text = "Desconectado"
+                self.statusLabel.textColor = .red
+            } else {
+                self.statusLabel.text = "Conectado"
+                self.statusLabel.textColor = .black
+            }
+        }
+    }
+    
+    func sendMqttMessage(string: String){
+        let data = string.data(using: .utf8)
+        if let mqtt = self.mqttSession{
+            mqtt.publish(data!, in: "/italus", delivering: .atLeastOnce, retain: false) { (result, error) in
+            }
+        }
+        
+    }
+    
 }
 
---
+++ CasaDigital/Pods/Target Support Files/Pods-CasaDigital/Pods-CasaDigital-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CasaDigital : NSObject
+@end
+@implementation PodsDummy_Pods_CasaDigital
+@end
